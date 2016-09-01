276051fd6eedb71621d488be053f1bd5aaf58c8d
BrunoDF <bdfachine@gmail.com>
2016-09-01 00:55:07 -0300

    Inicia DB no main

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"wedd-in/models"
 	_ "wedd-in/routers"
 
 	"github.com/astaxie/beego"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
+
+	// Swagger
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	// Database startup
+	models.InitDB()
+
+	// Database close on end
+	defer models.DB.Close()
+
+	// Run
 	beego.Run()
+
 }
